e802e94603a1bfb1d952cf48fcc87cb61b7f843d
Eduardo Bubniaki <148967335+Bubniaki@users.noreply.github.com>
2024-09-26 17:03:26 -0300

    Add files via upload

---
+++ flashcard-aula_10 (2)/flashcard-aula_10/perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Futebol',
+    'Em que times o Ronaldo fenomeno jogol?',
+    'Cruzeiro, PSV Eindhoven, Barcelona, Internazionale, Real Madrid, Milan e Corinthians'
+)
+
+criaCartao(
+    'Geografia',
+    'Países que fazem fronteira com a Itália?',
+    'França, Suíça, Áustralia e Eslovénia'
+)
+
+criaCartao(
+    'Literatura',
+    'Qual o detetive mais conhecido de Londres?',
+    'Sherlock Holmes'
+)
+
+criaCartao(
+    'história',
+    'Quem comandava o exército espartano?',
+    'Leonidas'
+)
